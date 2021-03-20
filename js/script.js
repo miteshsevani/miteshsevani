160f4e37d7ee72445a197d216b4faa8661c64905
Mitesh Sevani <msevani@gmail.com>
2021-03-20 23:10:03 +0000

    Performance enhancements and code cleanup

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelector(".navbar-toggle").addEventListener("click", () => {
+  document.querySelector("#navbar-collapse").classList.toggle("collapse");
+});
+
+document.querySelector(".navbar-nav").addEventListener("click", () => {
+  document.querySelector("#navbar-collapse").classList.toggle("collapse");
+});
